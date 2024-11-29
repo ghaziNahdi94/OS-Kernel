e0fb528b2cc32278c0da284239baa2fd7997148f
Ghazi <ghazinehdi@gmail.com>
2024-11-29 21:52:49 +0100

    add print method to the kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,68 @@
 
 #include "kernel.h"
+#include <stdint.h>
+#include <stddef.h>
+
+uint16_t* video_mem = (uint16_t*) (0xB8000);
+int row = 0;
+int column = 0;
+char color = 11;
 
 void kernel_main() {
-    char* video_mem = (char*) (0xB8000);
 
-    char* name = "Ghazi NEHDI";
+    init_terminal();
 
-    int memory_index = 0;
-    for(int i=0; i<11; i++) {
-        video_mem[memory_index] = name[i];
-        video_mem[memory_index + 1] = 12;
-        memory_index += 2;
+    print("CLUB AFRICAIN 1920\n");
+    print("Ghazi\n");
+    print("Nehdi\n");
+}
+
+void init_terminal() {
+    row = 0;
+    column = 0;
+
+    for(int line=0; line<VGA_HEIGHT; line++) {
+        for(int column=0; column<VGA_WIDTH; column++) {
+            video_mem[line * VGA_WIDTH + column] = terminal_make_char(' ', 1);
+        }
+    }
+}
+
+uint16_t terminal_make_char(char character, char color) {
+    return (color << 8) | character;
+}
+
+size_t strlen(const char* string) {
+    size_t length = 0;
+    while(string[length]) {
+        length++;
+    }
+
+    return length;
+}
+
+void terminal_put_char(int column, int row, char character, char color) {
+    video_mem[row * VGA_WIDTH + column] = terminal_make_char(character, color);    
+}
+
+void terminal_print_char(char character, char color) {
+    if(character == '\n') {
+        row++;
+        column = 0;
+        return;
+    }
+
+    terminal_put_char(column, row, character, color);
+    column++;
+
+    if(column == VGA_WIDTH) {
+        row++;
+        column = 0;
+    }
+}
+
+void print(char* string) {
+    for(int i=0; i<strlen(string); i++) {
+        terminal_print_char(string[i], color);
     }
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,4 +1,17 @@
 #ifndef KERNEL_H
 #define KERNEL_H
+
+    #include <stdint.h>
+    #include <stddef.h>
+
+    #define VGA_WIDTH 80
+    #define VGA_HEIGHT 20
+
+    void init_terminal();
     void kernel_main();
+    uint16_t terminal_make_char(char character, char color);
+    size_t strlen(const char* string);
+    void terminal_put_char(int column, int row, char character, char color);
+    void terminal_print_char(char character, char color);
+    void print(char* string);
 #endif
