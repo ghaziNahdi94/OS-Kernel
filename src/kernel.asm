31593c14dbbd89799e5369b62436c2aff040a017
Ghazi <ghazinehdi@gmail.com>
2024-12-02 18:32:12 +0100

    use assembly to display a message using VGA memory

--- src/kernel.asm
+++ src/kernel.asm
@@ -21,7 +21,45 @@ _start:
     or al, 2
     out 0x92, al
 
-    call kernel_main
+    call clearAll
+    mov ecx, message
+    call print
+    jmp $
+
+print:
+    mov ebx, 0xB8000
+.iteration_print:
+    mov al, byte [ecx]
+    cmp al, 0
+    je .finish_print
+    mov byte [ebx], al
+    mov al, 11
+    mov byte [ebx + 1], al
+    add ebx, 2
+    inc ecx
+    jmp .iteration_print
+.finish_print:
+    ret
+
+clearAll:
+    mov ebx, 0xB8000
+    mov dl, 0
+.iteration_clearAll:
+    cmp dl, 400
+    je .finish_clearAll
+    mov al, ' '
+    mov byte [ebx], al
+    mov al, 1
+    mov byte [ebx + 1], al
+    add ebx, 2
+    inc dl
+    jmp .iteration_clearAll
+.finish_clearAll:
+    ret
+
+
+    ;call kernel_main
     jmp $
 
 times 512-($ - $$) db 0
+message db 'Hello world !', 0
